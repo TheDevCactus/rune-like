6ea6eed0f64ba72d9d3cbdf26fc500e47656f243
Cactus <morganb816@gmail.com>
2023-04-22 03:30:38 -0400

    chore: store

--- stuffed/src/main.rs
+++ stuffed/src/main.rs
@@ -5,6 +5,7 @@ const COLON: &str = ":";
 const SEMICOLON: &str = ";";
 const COMMA: &str = ",";
 const ROLL_OUT: &str = ">";
+const ARRAY: &str = "[]";
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
@@ -18,6 +19,7 @@ pub enum Token {
     Semicolon,
     Comma,
     RollOut,
+    Array
 }
 
 impl From<String> for Token {
@@ -30,6 +32,7 @@ impl From<String> for Token {
             SEMICOLON => Token::Semicolon,
             COMMA => Token::Comma,
             ROLL_OUT => Token::RollOut,
+            ARRAY => Token::Array,
             _ => Token::ContextRequired(s),
         }
     }
@@ -45,6 +48,7 @@ impl Into<String> for Token {
             Token::Colon => COLON.to_string(),
             Token::Comma => COMMA.to_string(),
             Token::Semicolon => SEMICOLON.to_string(),
+            Token::Array => ARRAY.to_string(),
             Token::ContextRequired(s) => s,
             Self::Ident(s) => s,
             Self::Value(n) => n.to_string(),
@@ -62,6 +66,7 @@ enum Property {
 struct Structure {
     name: String,
     nodes: Vec<Property>,
+    array_indexes: Vec<bool>,
 }
 
 fn main() {
@@ -100,6 +105,7 @@ fn main() {
     let mut current_structure = Structure {
         name: String::new(),
         nodes: Vec::new(),
+        array_indexes: Vec::new(),
     };
     let mut current_property_type = Property::OneOf(String::new());
     let mut current_property = Token::Ident(String::new());
@@ -110,6 +116,7 @@ fn main() {
                 current_structure = Structure {
                     name: String::new(),
                     nodes: Vec::new(),
+                    array_indexes: Vec::new(),
                 };
             }
             Token::Ident(s) => {
@@ -155,11 +162,23 @@ fn main() {
                     0.to_string()
                 );
             }
+            Token::Array => {
+                current_structure.array_indexes.push(true);
+            }
+            Token::Comma => {
+                if current_structure.array_indexes.len() < current_structure.nodes.len() {
+                    current_structure.array_indexes.push(false);
+                }
+            }
             Token::Semicolon => {
+                if current_structure.array_indexes.len() < current_structure.nodes.len() {
+                    current_structure.array_indexes.push(false);
+                }
                 structures.push(current_structure);
                 current_structure = Structure {
                     name: String::new(),
                     nodes: Vec::new(),
+                    array_indexes: Vec::new(),
                 };
             }
             _ => {}
